7a019115beda106748e84f2498d04900d6efcdd0
Sarah Farrell <sarahfarrell11@yahoo.co.uk>
2019-07-19 20:46:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function sayHiToGrandma(string){
   if (string.toLowerCase() === string)
   return "I can't hear you!" 
   
-  if (string.toUpperCase() === string)
+  if (string.toUpperCase() === string) 
   return "YES INDEED!" }
 
   if (string.toLowerCase() === string) {
