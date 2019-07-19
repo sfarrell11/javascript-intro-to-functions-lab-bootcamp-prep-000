c1155fa9822716c0927ae8fac7ee6d0265fe6e39
Sarah Farrell <sarahfarrell11@yahoo.co.uk>
2019-07-19 20:10:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,6 +19,6 @@ function sayHiToGrandma(string){
   if (string.toUpperCase() === string)
   return "YES INDEED!"
 }
-  function sayHiToGrandma(string) {
-  if (string.toLowerCase() === string)
+  function sayILoveYouGrandma(string) {
+  if (string.sayILoveYouGrandma() === string)
   return "I love you, too" }
