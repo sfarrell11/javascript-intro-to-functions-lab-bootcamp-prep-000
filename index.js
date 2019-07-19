c385512db62977c2e6bb20fa74cf1af0ef2c3438
Sarah Farrell <sarahfarrell11@yahoo.co.uk>
2019-07-19 19:07:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ return string() ("I CAN'T HEAR YOU")stringtoLowerCase())
 }
 {return string () ("YES INDEED!") if string.toUpperCase())
 }
- return string ("I love you, too") if string ("I love you, too")
+{return string ("I love you, too") if string ("I love you, too")
 }
-} return string ("I love you, too") if string ("I love you, too")
+ return string ("I love you, too") if string ("I love you, too")
 
 
