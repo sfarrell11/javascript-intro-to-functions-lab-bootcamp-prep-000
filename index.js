9aaf975550d06311aabbbb1827757c9deea7a326
Sarah Farrell <sarahfarrell11@yahoo.co.uk>
2019-07-19 21:04:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,14 +12,3 @@ function logShout(string) {
 function logWhisper(string) {
 console.log(string.toLowerCase())
 }
-function sayHiToGrandma(string){
-  if (string.toLowerCase() === string)
-  return "I can't hear you!" 
-}
-  if (string.toUpperCase() === string) 
-  { return "YES INDEED!" }
-
-  if (string.toLowerCase() === string) {
-  return  "I love you, too" 
- }
-    }
