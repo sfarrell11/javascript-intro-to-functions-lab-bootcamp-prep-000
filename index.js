66750a39c8ed4c4c27981afe772029127660f325
Sarah Farrell <sarahfarrell11@yahoo.co.uk>
2019-07-19 19:08:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,12 +13,12 @@ function logWhisper(string) {
 console.log(string.toLowerCase())
 }
 function sayHiToGrandma () {
-return string() ("I CAN'T HEAR YOU")stringtoLowerCase())
+return string ("I CAN'T HEAR YOU")stringtoLowerCase())
 }
-{return string () ("YES INDEED!") if string.toUpperCase())
+{return string ("YES INDEED!") if string.toUpperCase())
+}
+{return string ("I love you, too") if string ("I love you, too")
 }
 {return string ("I love you, too") if string ("I love you, too")
 }
-{return string ("I love you, too") if string ("I love you, too")
-
 
